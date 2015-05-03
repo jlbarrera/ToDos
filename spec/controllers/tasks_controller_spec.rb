becb2409f0f2d44670102506e63f5e0e2369c0f4
José Luis Barrera <jl.barrera.trancoso@gmail.com>
2015-05-03 19:38:37 +0200

    Updating controller spec

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
 
+    describe "Creating new Task" do
+
+        let!(:task) { FactoryGirl.create(:task) }
+
+        it "should increment the tasks count" do
+            expect{ post :create, task: FactoryGirl.attributes_for(:task) }.to change{ Task.count }.by(1)
+        end
+
+        it "should respond with success" do
+            post :create, task: FactoryGirl.attributes_for(:task)
+            expect(response).to be_success
+        end
+    end
+
 end
