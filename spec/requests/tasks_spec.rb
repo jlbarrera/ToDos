1848ad7af5db348a18893c19dfd260ecd3d7893f
José Luis Barrera <jl.barrera.trancoso@gmail.com>
2015-05-03 18:34:23 +0200

    Updating request specs

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -12,11 +12,15 @@ RSpec.describe "Tasks", type: :request do
         expect(response).to be_success
     end
 
-    it "should list all open tasks" do
-        expect(json.collect{|l| l["status"]}).to include("Open")
+    it "should have all open tasks" do
+        tasks.each do |task|
+            if task.status.eql?("Open")
+                expect(json.collect{|l| l["title"]}).to include(task.title)
+            end
+        end
     end
 
-    it "should not list closed tasks" do
+    it "should not have any closed tasks" do
         expect(json.collect{|l| l["status"]}).not_to include("Closed")
     end
   end
@@ -28,28 +32,115 @@ RSpec.describe "Tasks", type: :request do
         expect(response).to be_success
     end
 
-    it "should list all closed tasks" do
-        expect(json.collect{|l| l["status"]}).to include("Closed")
+    it "should have all closed tasks" do
+        tasks.each do |task|
+            if task.status.eql?("Closed")
+                expect(json.collect{|l| l["title"]}).to include(task.title)
+            end
+        end
     end
 
-    it "should not list open tasks" do
+    it "should not have any open tasks" do
         expect(json.collect{|l| l["status"]}).not_to include("Open")
     end
   end
 
+  describe "GET /tasks/all" do
+
+    before do
+        get tasks_all_path
+        expect(response).to be_success
+    end
+
+    it "should have all tasks" do
+        tasks.each do |task|
+            expect(json.collect{|l| l["title"]}).to include(task.title)
+        end
+        expect(json.count).to match(3)
+    end
+  end
+
   describe "GET /task/:id" do
+
+    let!(:task) { tasks.first }
+
+    context "valid task" do
+        before do
+            get task_path task
+            expect(response).to be_success
+        end
+
+        it "should have task info" do
+            expect(json["title"]).to eq(task.title)
+        end
+
+        it "should not have any other task" do
+            expect(json.kind_of?(Array)).to eq(false)
+        end
+    end
+
+    context "invalid task" do
+
+        before do
+            get task_path(99)
+            expect(response).to be_success
+        end
+
+        it "should alert us about the error" do
+            expect(json["error"]).to eq("The task doesn't exist")
+        end
+    end
   end
 
-  describe "GET /task/:id/edit" do
-  end
 
-  describe "GET /task/new" do
-  end
+  describe "POST /tasks" do
 
-  describe "POST /task/:id" do
+    let!(:task) { FactoryGirl.create(:task) }
+
+    context "with valid data" do
+
+        before do
+            @task_attributes = FactoryGirl.attributes_for(:task)
+            post "/tasks", :task => @task_attributes
+            expect(response).to be_success
+        end
+
+        it "should have the new task info" do
+            expect(json["title"]).to eq(@task_attributes[:title])
+        end
+
+        it "should not have any other task" do
+            expect(json.kind_of?(Array)).to eq(false)
+        end
+    end
+
   end
 
   describe "DELETE /task/:id" do
+
+    let!(:task) { tasks.first }
+
+    context "with valid data" do
+        before do
+            delete "/tasks/#{task.id}"
+            expect(response).to be_success
+        end
+
+        it "should confirm us that the task have been deleted" do
+            expect(json["success"]).to eq("The task have been deleted")
+        end
+    end
+
+    context "with not valid data" do
+        before do
+            delete "/tasks/99"
+            expect(response).to be_success
+        end
+
+        it "should alert about the error" do
+            expect(json["error"]).to eq("The task doesn't exist")
+        end
+    end
   end
 
 end
